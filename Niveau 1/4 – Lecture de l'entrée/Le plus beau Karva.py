d68aae96b73ea0f547dfaaeb0e77aadc2c60c70c
Baptiste Zahnow <75901978+LelouchFR@users.noreply.github.com>
2022-09-19 10:53:13 +0200

    Create Le plus beau Karva.py

---
+++ Niveau 1/4 – Lecture de l'entrée/Le plus beau Karva.py
@@ -0,0 +1,9 @@
+nombreKarva = int(input())
+  
+for i in range(nombreKarva):
+   poids = int(input())
+   i = int(input())
+   longueur = int(input())
+   hauteur = int(input())
+   resultat = longueur * hauteur + poids
+   print(resultat)
