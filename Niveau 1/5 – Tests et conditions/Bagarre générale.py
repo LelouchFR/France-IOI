538d1e4e0bfdcc8b8f5be944c2d601c1b0e3752d
Baptiste Zahnow <75901978+LelouchFR@users.noreply.github.com>
2022-09-19 12:01:28 +0200

    Create Bagarre générale.py

---
+++ Niveau 1/5 – Tests et conditions/Bagarre générale.py
@@ -0,0 +1,14 @@
+champEva = int(input())
+champAri = int(input())
+
+if champAri > champEva:
+   if champAri - champEva > 10:
+      print("La famille Evaran a un champ trop grand")
+   else:
+      print()
+   
+elif champEva > champAri:
+   if champEva - champAri > 10:
+      print("La famille Arignon a un champ trop grand")
+   else:
+      print()
