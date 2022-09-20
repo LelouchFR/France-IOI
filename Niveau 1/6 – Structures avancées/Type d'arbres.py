a52309b2b8511638327d50d9274a76be3c36452b
Baptiste Zahnow <75901978+LelouchFR@users.noreply.github.com>
2022-09-20 13:25:58 +0200

    Create Type d'arbres.py

---
+++ Niveau 1/6 – Structures avancées/Type d'arbres.py
@@ -0,0 +1,11 @@
+height = int(input())
+folioles = int(input())
+
+if height <= 5 and folioles >= 8:
+   print("Tinuviel")
+if height >= 10 and folioles >= 10:
+   print("Calaelen")
+if height <= 8 and folioles <= 5:
+   print("Falarion")
+if height >= 12 and folioles <= 7:
+   print("Dorthonion")
