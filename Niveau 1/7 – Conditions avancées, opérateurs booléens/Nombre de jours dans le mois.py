9a690fef3c9b23c45f6e15bc547660522a4b3f55
Baptiste Zahnow <75901978+LelouchFR@users.noreply.github.com>
2022-09-20 17:47:04 +0200

    Create Nombre de jours dans le mois.py

---
+++ Niveau 1/7 – Conditions avancées, opérateurs booléens/Nombre de jours dans le mois.py
@@ -0,0 +1,8 @@
+uX = int(input())
+
+if uX == 1 or uX == 2 or uX == 3 or uX == 7 or uX == 8 or uX == 9:
+   print(30)
+elif uX == 4 or uX == 5 or uX == 6 or uX == 10:
+   print(31)
+else:
+   print(29)
